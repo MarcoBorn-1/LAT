7ed4f947f0fbe8908468eb46635b5365d092ab5f
Marco Born <marco.born345@gmail.com>
2024-05-04 15:52:57 +0200

    Enabled JPA and added H2 database info into properties

--- src/main/java/com/example/lat/LatApplication.java
+++ src/main/java/com/example/lat/LatApplication.java
@@ -2,10 +2,11 @@ package com.example.lat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.example.lat.repository")
 public class LatApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(LatApplication.class, args);
 	}
