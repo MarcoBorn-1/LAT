49578299e14864c2587f4f9652afd7e408d26afa
Marco Born <marco.born345@gmail.com>
2024-05-06 17:27:46 +0200

    Added exception handling for incorrect usage of promo code type and date format

--- src/main/java/com/example/lat/config/GlobalExceptionHandler.java
+++ src/main/java/com/example/lat/config/GlobalExceptionHandler.java
@@ -3,11 +3,13 @@ package com.example.lat.config;
 import com.example.lat.exception.MissingValueException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import java.time.format.DateTimeParseException;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
@@ -32,5 +34,15 @@ public class GlobalExceptionHandler {
     @ResponseBody
     public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("An error occured: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid request body: " + ex.getMessage());
+    }
+
+    @ExceptionHandler(DateTimeParseException.class)
+    public ResponseEntity<String> handleDateTimeParseException(DateTimeParseException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Invalid date format. Please use the format 'dd.MM.yyyy'.");
     }
 }
